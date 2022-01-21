ae9b67d4708704f24ea7d27bce4e268e90ed0eb8
Rafael Nelson <rafaelson1337@gmail.com>
2022-01-21 00:49:38 -0300

    fully working version

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-const wrapper = document.querySelector(".wrapper");
+const wrapper = document.querySelector(".grid-wrapper");
+const button = document.querySelector(".button");
+button.addEventListener("click", () => {
+  let size = prompt(
+    "Insert the number of squares per side (maximum of 100)",
+    0
+  );
+  if (size > 100) {
+    return;
+  }
+  deleteGrid();
+  createGrid(size);
+});
 
 function createSquare() {
   const div = document.createElement("div");
-  div.setAttribute("class", "square");
+  div.classList.add("square");
+  div.addEventListener(
+    "mouseover",
+    () => (div.style.backgroundColor = "black")
+  );
   wrapper.appendChild(div);
-  document.querySelector(".wrapper").appendChild(div);
+  document.querySelector(".grid-wrapper").appendChild(div);
 }
 
 function createGrid(n) {
-  for (i = 0; i < n; i++) {
+  wrapper.style.gridTemplateColumns = `repeat(${n}, auto)`;
+  for (i = 0; i < n * n; i++) {
     createSquare();
   }
+}
+
+function deleteGrid() {
+  let squares = document.querySelectorAll(".square");
+  squares.forEach((sqr) => sqr.remove());
 }
